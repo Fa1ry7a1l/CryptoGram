639d9d8ec9b22682212a1966bc0fceadb0fe489f
thecattest <vodopyanov99@gmail.com>
2021-01-08 12:56:40 +0300

    simple list view

--- app/src/main/java/com/sushko/myapplication/MainActivity.java
+++ app/src/main/java/com/sushko/myapplication/MainActivity.java
@@ -3,6 +3,8 @@ package com.sushko.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView lv = findViewById(R.id.listView);
+        String[] names = getResources().getStringArray(R.array.names);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, names);
+        lv.setAdapter(adapter);
     }
 }
